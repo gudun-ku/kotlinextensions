2a7f65026295c451ed67966118545084c53dd6a1
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-12-13 20:20:17 +0300

    Adds getEven for randomInts 30:23

--- app/src/main/java/ru/skillbranch/extensions/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/extensions/MainActivity.kt
@@ -7,6 +7,7 @@ import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.extensions.utils.setPaddingOptionally
 import ru.skillbranch.extensions.utils.span
 
 class MainActivity : AppCompatActivity() {
@@ -15,12 +16,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // usual way
+//        some_text_view.text =
+//            SpannableStringBuilder()
+//                .append("new text", ForegroundColorSpan(ContextCompat.getColor(this, R.color.red)),
+//                    SpannableString.SPAN_INCLUSIVE_EXCLUSIVE)
+//                .append(" ")
+//                .append("\n")
+//                .append("new text", ForegroundColorSpan(ContextCompat.getColor(this, R.color.blue)),
+//                    SpannableString.SPAN_INCLUSIVE_EXCLUSIVE)
 
-        some_text_view.text =
-            SpannableStringBuilder()
-                .append("new text", ForegroundColorSpan(ContextCompat.getColor(this, R.color.red)),
-                    SpannableString.SPAN_INCLUSIVE_EXCLUSIVE)
-
+        // using extensions
         some_text_view.text = span {
             color("This string will be RED", R.color.red)
             space()
@@ -31,5 +37,10 @@ class MainActivity : AppCompatActivity() {
                 bold()
             }
         }
+
+        some_text_view.setPaddingOptionally(
+            top = 16,
+            left = 16
+        )
     }
 }
--- app/src/main/java/ru/skillbranch/extensions/datasource/RandomInts.kt
+++ app/src/main/java/ru/skillbranch/extensions/datasource/RandomInts.kt
@@ -6,3 +6,18 @@ class RandomInts {
 
     fun next(): Int = Random.nextInt()
 }
+
+// хотим складывать только четные числа которые берем из генератора 30:23
+
+fun RandomInts.getEven(): Int? =
+    next().takeIf { it % 2 == 0 }
+
+fun sampleRandom() {
+    val gen = RandomInts()
+
+    val a: Int? = gen.getEven()
+    val b = gen.getEven()
+    val c = gen.getEven()
+
+    val result = a + b + c
+}
